fc28fca7dd1161f37510bc92dc88dcf3d896b43f
Marc B <125284318+M-Busk@users.noreply.github.com>
2024-01-17 15:19:46 +0100

    javadocs

--- src/main/java/eu/merloteducation/gxfscataloglibrary/service/GxfsCatalogService.java
+++ src/main/java/eu/merloteducation/gxfscataloglibrary/service/GxfsCatalogService.java
@@ -21,18 +21,43 @@ public class GxfsCatalogService {
     @Autowired
     private GxfsSignerService gxfsSignerService;
 
+    /**
+     * Given the hash of a self-description in the catalog, set its status in the catalog to revoked.
+     *
+     * @param sdHash hash of the SD
+     * @return SD meta response of the catalog
+     */
     public SelfDescriptionMeta revokeSelfDescriptionByHash(String sdHash) {
         return this.gxfsCatalogClient.postRevokeSelfDescriptionByHash(sdHash);
     }
 
+    /**
+     * Given the hash of a self-description in the catalog, fully delete/purge it from the catalog.
+     *
+     * @param sdHash hash of the SD
+     */
     public void deleteSelfDescriptionByHash(String sdHash) {
         this.gxfsCatalogClient.deleteSelfDescriptionByHash(sdHash);
     }
 
+    /**
+     * Given the id of a participant in the catalog, return the respective item including the self-description.
+     *
+     * @param participantId hash of the SD
+     * @return catalog content of the participant
+     */
     public ParticipantItem getParticipantById(String participantId) {
         return this.gxfsCatalogClient.getParticipantById(participantId);
     }
 
+    /**
+     * Given a list of ids of self-descriptions in the catalog, return a list of self-description items that match these ids.
+     * By default, this method will only return SDs with the status ACTIVE.
+     * If other statuses are required, use the overloaded function with the same name.
+     *
+     * @param ids array of ids to query the catalog for
+     * @return list of SD items that match the ids
+     */
     public GXFSCatalogListResponse<SelfDescriptionItem> getSelfDescriptionsByIds(String[] ids) {
         return this.gxfsCatalogClient.getSelfDescriptionList(
                 null,
@@ -48,6 +73,14 @@ public class GxfsCatalogService {
                 ids.length);
     }
 
+    /**
+     * Given a list of ids of self-descriptions in the catalog, return a list of self-description items that match these ids.
+     * This overload allows to further specify which states the SDs shall have in the catalog.
+     *
+     * @param ids array of ids to query the catalog for
+     * @param selfDescriptionStatuses array of wanted SD statuses
+     * @return list of SD items that match the ids
+     */
     public GXFSCatalogListResponse<SelfDescriptionItem> getSelfDescriptionsByIds(String[] ids,
                                                                                  SelfDescriptionStatus[] selfDescriptionStatuses) {
         return this.gxfsCatalogClient.getSelfDescriptionList(
@@ -64,6 +97,14 @@ public class GxfsCatalogService {
                 ids.length);
     }
 
+    /**
+     * Given a list of hashes of self-descriptions in the catalog, return a list of self-description items that match these hashes.
+     * By default, this method will only return SDs with the status ACTIVE.
+     * If other statuses are required, use the overloaded function with the same name.
+     *
+     * @param hashes array of hashes to query the catalog for
+     * @return list of SD items that match the hashes
+     */
     public GXFSCatalogListResponse<SelfDescriptionItem> getSelfDescriptionsByHashes(String[] hashes) {
         return this.gxfsCatalogClient.getSelfDescriptionList(
                 null,
@@ -79,6 +120,14 @@ public class GxfsCatalogService {
                 hashes.length);
     }
 
+    /**
+     * Given a list of hashes of self-descriptions in the catalog, return a list of self-description items that match these hashes.
+     * This overload allows to further specify which states the SDs shall have in the catalog.
+     *
+     * @param hashes array of hashes to query the catalog for
+     * @param selfDescriptionStatuses array of wanted SD statuses
+     * @return list of SD items that match the hashes
+     */
     public GXFSCatalogListResponse<SelfDescriptionItem> getSelfDescriptionsByHashes(String[] hashes,
                                                                                     SelfDescriptionStatus[] selfDescriptionStatuses) {
         return this.gxfsCatalogClient.getSelfDescriptionList(
@@ -95,6 +144,13 @@ public class GxfsCatalogService {
                 hashes.length);
     }
 
+    /**
+     * Given the credential subject of a self-description that inherits from gax-core:ServiceOffering,
+     * wrap it in a verifiable presentation, sign it using the key that was provided to this library and send it to the catalog.
+     *
+     * @param serviceOfferingCredentialSubject service offering credential subject to insert into the catalog
+     * @return SD meta response of the catalog
+     */
     public SelfDescriptionMeta addServiceOffering(
             GaxCoreServiceOfferingCredentialSubject serviceOfferingCredentialSubject) throws Exception {
         VerifiablePresentation vp = gxfsSignerService
@@ -104,6 +160,13 @@ public class GxfsCatalogService {
         return this.gxfsCatalogClient.postAddSelfDescription(vp);
     }
 
+    /**
+     * Given the credential subject of a self-description that inherits from gax-trust-framework:LegalPerson,
+     * wrap it in a verifiable presentation, sign it using the key that was provided to this library and send it to the catalog.
+     *
+     * @param participantCredentialSubject participant credential subject to insert into the catalog
+     * @return catalog content of the participant
+     */
     public ParticipantItem addParticipant(GaxTrustLegalPersonCredentialSubject participantCredentialSubject)
             throws Exception {
         VerifiablePresentation vp = gxfsSignerService
@@ -113,6 +176,15 @@ public class GxfsCatalogService {
         return this.gxfsCatalogClient.postAddParticipant(vp);
     }
 
+    /**
+     * Given the credential subject of a self-description that inherits from gax-trust-framework:LegalPerson
+     * and whose id already exists in the catalog, wrap it in a verifiable presentation,
+     * sign it using the key that was provided to this library and send it to the catalog
+     * to update it.
+     *
+     * @param participantCredentialSubject participant credential subject to update in the catalog
+     * @return catalog content of the participant
+     */
     public ParticipantItem updateParticipant(GaxTrustLegalPersonCredentialSubject participantCredentialSubject)
             throws Exception {
         VerifiablePresentation vp = gxfsSignerService
@@ -124,6 +196,15 @@ public class GxfsCatalogService {
                 vp);
     }
 
+    /**
+     * Given paging parameters, return a list (page) of participant URIs corresponding to these parameters.
+     * This is mainly used to access the participant self-descriptions in a second step, by using the
+     * uris as IDs and requesting SDs with these IDs.
+     *
+     * @param offset paging offset
+     * @param size page size
+     * @return list of participant uris corresponding to the paging parameters
+     */
     public GXFSCatalogListResponse<GXFSQueryUriItem> getParticipantUriPage(long offset, long size) {
         String query = """
                 {
