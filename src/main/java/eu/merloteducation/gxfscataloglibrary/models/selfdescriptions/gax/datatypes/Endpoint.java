5e6b1a9cbef794bdc0b8286f6e0eab44d8ded3cd
Marc B <125284318+M-Busk@users.noreply.github.com>
2024-01-25 13:11:01 +0100

    remove unused imports

--- src/main/java/eu/merloteducation/gxfscataloglibrary/models/selfdescriptions/gax/datatypes/Endpoint.java
+++ src/main/java/eu/merloteducation/gxfscataloglibrary/models/selfdescriptions/gax/datatypes/Endpoint.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import eu.merloteducation.gxfscataloglibrary.models.serialization.StringDeserializer;
-import eu.merloteducation.gxfscataloglibrary.models.serialization.StringSerializer;
 import eu.merloteducation.gxfscataloglibrary.models.serialization.UriDeserializer;
 import eu.merloteducation.gxfscataloglibrary.models.serialization.UriSerializer;
 import jakarta.validation.constraints.NotNull;
